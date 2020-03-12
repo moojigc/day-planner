9a32ac2d01374645c8a188688246f92ccbdc5192
Moojig <moojigc@gmail.com>
2020-03-12 00:27:51 -0400

    Most functionality is programmed. In this version, current time is permanently set to Noon for color dev purposes. Still working on actually being able to edit and save the user's plans with the modal.

--- assets/script.js
+++ assets/script.js
@@ -15,34 +15,164 @@ function printCurrentTime() {
 }
 printCurrentTime();
 
-// Generate the rows bc I didn't wanna write the html lol
-function generateTimeBlocks() {
-    var hours = ["9","10","11","12","13","14","15","16","17","18"];
-    var timeBlockDiv = $("<div>").addClass("row shadow");
-    var timeDisplay = $("<div>").addClass("timeDisplay col-lg-1");
-    var plansDisplay = $("<div>").addClass("plansDisplay col-lg-10");
-    var editorIcon = $("<button>").addClass("editorIcon btn col-lg-1");
-    var calIcon = $("<i>").addClass("fas fa-calendar-plus")
+// Generate the rows dynamically
+function appendTimeBlocks(newTimeBlocks, appendOrPrepend) {
+    var hours = [];
+    // if (newTimeBlocks !== undefined) {
+        // hours = [];
+        hours.push(newTimeBlocks); //allows user to add more time blocks if they want
+    // }
+    var timeBlockDiv;
+    var hourDisplay;
+    var plansDisplay;
+    var editorIcon;
+    var calIcon;
+    
+    function createATimeBlock() {
+        timeBlockDiv = $("<div>").addClass("row shadow");
+        hourDisplay = $("<div>").addClass("hourDisplay col-md-1");
+        plansDisplay = $("<div>").addClass("plansDisplay col-md-10");
+        editorIcon = $("<button>").addClass("editorIcon btn col-md-1");
+        editorIcon.attr("data-toggle", "modal"); editorIcon.attr("data-target", "#plansEditorModal");
+        calIcon = $("<i>").addClass("fas fa-calendar-plus");    
+    }
 
-    for(i=0; i<hours.length; i++) {
-        if (parseInt(hours[i]) < 13) {
-            timeDisplay.text(hours[i] + " AM");
-            editorIcon.append(calIcon);
-            plansDisplay.attr("data-hour" + i);
-            timeBlockDiv.append(timeDisplay, plansDisplay, editorIcon);
-            wrapper.append(timeBlockDiv);
-            console.log("also running");
-        } else {
-            timeDisplay.text(12 - parseInt(hours[i]) + " PM");
-            editorIcon.append(calIcon);
-            plansDisplay.attr("data-hour" + i);
-            timeBlockDiv.append(timeDisplay, plansDisplay, editorIcon);
-            wrapper.append(timeBlockDiv);
-            console.log("running");
+    hours.forEach(hour => {
+        function isItEarlierOrLater() {
+            var currentRealHour = moment("12", "HH"); //Saves current hour (in 24hr format) as an integer
+            // var currentRealHour = 12;
+            // var currentHour = currentRealHour + beforeAfter;
+
+            if (moment(hour, "HH").isAfter(currentRealHour, "hour")) {
+                plansDisplay.attr("style", "background-color: #ff7272");
+            } else if (moment(hour, "HH").isBefore(currentRealHour, "hour")) {
+                plansDisplay.attr("style", "background-color: #35b9ba");
+            }
+
+            // Gradient time!
+            // if (currentHour - parseInt(hour) >= 9) {
+            //     plansDisplay.attr("style", "background-color: #ffba26");
+            // } 
+            // else if (currentHour - parseInt(hour) === 8) {
+            //     plansDisplay.attr("style", "background-color: #f3ba2f");
+            // } 
+            // else if (currentHour - parseInt(hour) === 7) {
+            //     plansDisplay.attr("style", "background-color: #e7ba38");
+            // } 
+            // else if (currentHour - parseInt(hour) === 6) {
+            //     plansDisplay.attr("style", "background-color: #d1ba48");
+            // } 
+            // else if (currentHour - parseInt(hour) === 5) {
+            //     plansDisplay.attr("style", "background-color: #b4ba5d");
+            // } 
+            // else if (currentHour - parseInt(hour) === 4) {
+            //     plansDisplay.attr("style", "background-color: #82ba82");
+            // } 
+            // else if (currentHour - parseInt(hour) === 3) {
+            //     plansDisplay.attr("style", "background-color: #67b995");
+            // } 
+            // else if (currentHour - parseInt(hour) === 2) {
+            //     plansDisplay.attr("style", "background-color: #4fb9a7");
+            // } 
+            // else if (currentHour - parseInt(hour) === 1) {
+            //     plansDisplay.attr("style", "background-color: #43b9b0");
+            // } 
+            // else if (currentHour === parseInt(hour)) {
+            //     plansDisplay.attr("style", " background: rgb(53,185,186); background: linear-gradient(0deg, rgba(53,185,186,1) 0%, rgba(255,186,38,1) 100%); ");
+            // } 
         }
-        
-    };
-}
-generateTimeBlocks();
+        if (hour < 13) {
+            createATimeBlock();
 
+            hourDisplay.text(hour + "AM");
+            editorIcon.append(calIcon);
+            // For use within the editor modal
+            editorIcon.attr("data-hour", moment(hour, "HH"));
+
+            timeBlockDiv.append(hourDisplay, plansDisplay, editorIcon);
+            plansDisplay.text(moment(hour, "HH"));
+
+            if (appendOrPrepend == "prepend") {
+                wrapper.prepend(timeBlockDiv);
+            } else {
+                wrapper.append(timeBlockDiv);
+            }
+            // console.log(hour + " AM");
+            isItEarlierOrLater();
+
+        } else {
+            createATimeBlock();
+        
+            hourDisplay.text(parseInt(hour) -12 + "PM");
+            editorIcon.append(calIcon);
+            // For use within the editor modal
+            editorIcon.attr("data-hour", moment(hour, "HH"));
+
+            timeBlockDiv.append(hourDisplay, plansDisplay, editorIcon);
+            plansDisplay.text(moment(hour, "HH"));
+
+            if (appendOrPrepend == "prepend") {
+                wrapper.prepend(timeBlockDiv);
+            } else {
+                wrapper.append(timeBlockDiv);
+            }
+            // console.log(parseInt(hour) - 12 + " PM");
+            isItEarlierOrLater();
+        }
+
+        console.log(editorIcon.data("hour"));
+        editUserPlans();
+    });
+}
+// console.log(timeBlockDiv.data("hour"));
+
+// To edit the plans
+function editUserPlans() {
+    $('#plansEditorModal').on('show.bs.modal', function (event) {
+        var button = $(event.relatedTarget) // Button that triggered the modal
+        var hour = button.data('hour') // Extract info from data-* attributes
+        if (button.parent().contents(".plansDisplay")[0].childNodes[0].data === undefined) {
+            return;
+        } else {
+            var thisPlanDisplay = button.parent().contents(".plansDisplay")[0].childNodes[0].data;
+        }
+        console.log(thisPlanDisplay);
+        console.log(hour);
+        
+        // Editing the modal content
+        var modal = $(this);
+        if (parseInt(hour)>12) {
+        modal.find('.modal-title').text(hour + "PM");
+        } else {
+            modal.find('.modal-title').text(hour + "AM");
+        }
+        $("#plansDescription").text(thisPlanDisplay);
+    })
+}
+
+// Calling functions
+function addTimeBlocks(newHours) {
+    newHours.forEach(hour => {
+        appendTimeBlocks(hour);
+    })
+}
+addTimeBlocks(["9","10","11","12","13","14","15","16","17"]);
+
+var newTimeBlockAfternoon = 17;
+$("#addTimeBlockBtnAfternoon").on("click", function() {
+    if (newTimeBlockAfternoon !== null && newTimeBlockAfternoon < 23) {
+        newTimeBlockAfternoon++;
+        appendTimeBlocks(newTimeBlockAfternoon);
+    }
+})
+var newTimeBlockMorning = 9;
+$("#addTimeBlockBtnMorning").on("click", function() {
+    if (newTimeBlockMorning !== null && newTimeBlockMorning > 1) {
+        newTimeBlockMorning--;
+        appendTimeBlocks(newTimeBlockMorning, "prepend");
+    }
+})
+
+
+// end of script
 });
